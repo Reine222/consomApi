70a5e13b36910281b3566faeb684e6ed630e8d24
Reine222 <47183434+Reine222@users.noreply.github.com>
2020-09-02 11:47:24 +0000

    testymoni terminé

--- projet/restaurant/restaurant/serializers.py
+++ projet/restaurant/restaurant/serializers.py
@@ -1,23 +1,27 @@
 from resto.models import *
 from rest_framework import serializers
 
+class TestimonySerializer(serializers.ModelSerializer):
+       
+    class Meta:
+        model = Testimony
+        fields = '__all__'
+
+        
+class PlatSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Plat
+        fields = '__all__'
+        depth = 1
 
 class CategorieSerializer(serializers.ModelSerializer):
+    catPlat = PlatSerializer(many=True, required=False)
+    
     class Meta:
         model = Categorie
         fields = '__all__'
 
 
-class PlatSerializer(serializers.ModelSerializer):
-    catPlat = CategorieSerializer(many=True, required=False)
-
-    class Meta:
-        model = Plat
-        fields = '__all__'
 
 
-class TestimonySerializer(serializers.ModelSerializer):
-   
-    class Meta:
-        model = Testimony
-        fields = '__all__'
